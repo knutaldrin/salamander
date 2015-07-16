765955b3e9da6ef841c17b520b4d0383618610a0
Knut Aldrin Wikström <knutaldrin@gmail.com>
2015-07-16 14:28:24 +0200

    Implemented socket, as well as get_connected_devices

--- SaleaeApi.py
+++ SaleaeApi.py
@@ -4,11 +4,7 @@ Api functions for Saleae Logic
 
 import socket
 
-__all__ = ['connect']
-
-# Possible responses from Logic
-ACK = 'ACK'
-NAK = 'NAK'
+__all__ = ['connect', 'SaleaeError', 'NAKError', 'ResponseError']
 
 
 def connect(host='127.0.0.1', port=10429):
@@ -24,7 +20,26 @@ def connect(host='127.0.0.1', port=10429):
     return _SaleaeSocket(host, port)
 
 
+class SaleaeError(Exception):
+    pass
+
+
+class NAKError(SaleaeError):
+    pass
+
+
+class ResponseError(SaleaeError):
+    pass
+
+
 class _SaleaeSocket(object):
+
+    #: Receive buffer size
+    _BUFSIZE = 1024
+
+    # Possible responses from Logic
+    _ACK = 'ACK'
+    _NAK = 'NAK'
 
     def __init__(self, host, port):
         """
@@ -36,7 +51,67 @@ class _SaleaeSocket(object):
         :type  port: int
         """
         try:
-            self.sock = socket.create_connection((host, port))
+            self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self._sock.connect((host, port))
+            self._sock.settimeout(2)
+
         except socket.error:
             # TODO: Handle error
             raise
+
+    def request(self, cmd):
+
+        try:
+            self._sock.send(cmd + '\0')
+
+            response = self._sock.recv(self._BUFSIZE).split('\n')
+
+            result = response.pop()
+
+            if result == 'NAK':
+                raise NAKError
+            elif result != 'ACK':
+                raise ResponseError('Response neither "ACK" nor "NAK"')
+
+            return response
+
+        except socket.error:
+            # TODO: Handle error
+            raise
+
+        except SaleaeError:
+            # TODO: Handle error
+            raise
+
+    def get_connected_devices(self):
+        """
+        Fetches a list of all connected Saleae devices.
+
+        :return: A list of dicts, containing:
+            * 'index':      Device index (starting from one)
+            * 'name':       Device name
+            * 'type':       Device type
+            * 'device_id':  64-bit device ID
+            * 'active':     If device is currently active
+        """
+
+        devices = []
+
+        for line in self.request('get_connected_devices'):
+
+            fields = line.split(', ')
+
+            if len(fields) < 4:
+                raise ResponseError('get_connected_devices returned < 4 fields')
+
+            active = (len(fields) == 5 and fields[4] == 'ACTIVE')
+
+            devices.append({
+                'index':     int(fields[0]),
+                'name':      fields[1],
+                'type':      fields[2],
+                'device_id': int(fields[3], base=0),
+                'active':    active
+            })
+
+        return devices
